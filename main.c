ed1625e41a95e364e0d3f5508c8e1d9315fcdc62
KrissM1987 <krissm@poczta.fm>
2018-10-12 11:15:24 +0200

    proby znaki 2 czesccxcxcxxcxcxc

--- main.c
+++ main.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 
 
-int main() {
+int main()
+{
 
 char znak;
-printf ("Wpisz znak : ");
+int wartosc_calkowita ;
+double wartosc_wymierna;
 
-znak = getchar();
+printf("Podaj znak : ");
+scanf(" %c", &znak);
+printf("\n Twoj znak to: %c", znak);
+printf("\nPodaj liczbe calkowita : ");
+scanf(" %i", &wartosc_calkowita);
+printf("\nLiczba calkowita %d", wartosc_calkowita);
+printf("\nPodaj liczbę wymierna : ");
+scanf(" %lf", &wartosc_wymierna);
+printf("\nliczba wymierna to : %lf", wartosc_wymierna);
 
-putchar(znak);
-printf("\ntwoj znak to : %d", znak);
+
+/*scanf(" %c%d%lf",
+        &znak,
+        &wartosc_calkowita,
+        &wartosc_wymierna);
+//printf ("Wpisz znak : ");
+//znak = getchar();
+//putchar(znak);
+ */
+printf("\nznak = %c" "\n liczba całkowita = %d""\n licza wymierna = %lf" ,
+        znak,
+        wartosc_calkowita,
+        wartosc_wymierna
+        );
 
 
 
