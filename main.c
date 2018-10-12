b8f4ce847e50c53ad8900cfa7b4e79cdcf824a16
KrissM1987 <krissm@poczta.fm>
2018-10-12 14:39:06 +0200

    proby znaki 3 podejscie

--- main.c
+++ main.c
@@ -3,35 +3,37 @@
 
 int main()
 {
-
+/*
 char znak;
 int wartosc_calkowita ;
 double wartosc_wymierna;
+char ciag_znakow[256];
 
 printf("Podaj znak : ");
 scanf(" %c", &znak);
 printf("\n Twoj znak to: %c", znak);
+printf("Podaj ciąg znakow :");
+scanf("%s", &ciag_znakow);
+printf("\nciag znakow to %s", ciag_znakow);
 printf("\nPodaj liczbe calkowita : ");
 scanf(" %i", &wartosc_calkowita);
 printf("\nLiczba calkowita %d", wartosc_calkowita);
 printf("\nPodaj liczbę wymierna : ");
 scanf(" %lf", &wartosc_wymierna);
 printf("\nliczba wymierna to : %lf", wartosc_wymierna);
+*/
 
-
-/*scanf(" %c%d%lf",
-        &znak,
-        &wartosc_calkowita,
-        &wartosc_wymierna);
-//printf ("Wpisz znak : ");
-//znak = getchar();
-//putchar(znak);
- */
+/*
 printf("\nznak = %c" "\n liczba całkowita = %d""\n licza wymierna = %lf" ,
         znak,
         wartosc_calkowita,
         wartosc_wymierna
         );
+*/
+
+char opcja ="x";
+char *Napis = "Opis programu";
+printf("Wybrano opcję %c: %21s", opcja, Napis);     //%21s wyrównanie do 21 znaków
 
 
 
